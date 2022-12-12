5bdb333b867f2f2030862321c4ccb12affd481e5
Eniade-Oluwademilade <demiladeeniade@gmail.com>
2022-12-12 11:39:29 -0800

    Data type sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * 
- * Return: Always 0 (success)
+ *
+ * Return: 0 (success)
  */
 int main(void)
 {
-printf("size of char: %lu byte(s)\n", sizeof(char));
-printf("size of int: %lu byte(s)\n", sizeof(int));
-printf("size of long int: %lu byte(s)\n", sizeof(long int));
-printf("size of long long int: %lu byte(s)\n", sizeof(long long int));
-printf("size of int float: %lu byte(s)\n", sizeof(int float));
-return (0);
+printf("size of char: %lu byte(s)\n",(unsigned long)sizeof(char));
+printf("size of int: %lu byte(s)\n", (unsigned long)sizeof(int));
+printf("size of long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
+printf("size of long long int: %lu byte(s)\n", (unsigned long)sizeof(long long int));
+printf("size of float: %lu byte(s)\n", (unsigned long)sizeof(float));
+return(0);
 }
+
