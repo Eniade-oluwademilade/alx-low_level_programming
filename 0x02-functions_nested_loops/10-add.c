5808a856142331fa03b3cad8757d9380ee2e0182
Eniade-Oluwademilade <demiladeeniade@gmail.com>
2022-12-14 15:05:26 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main
+ * function: adding numbers
+ * Return: 0 (success)
+ */
+int add(int, int)
+{return (int + int)}
+{
+int main(void)
+	_putchar("%d", add(3, 7);
+return (0);
+}
