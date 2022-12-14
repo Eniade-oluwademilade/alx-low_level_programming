7022274785b71a0fd42fefdcd0b03f558caf104b
Eniade-Oluwademilade <demiladeeniade@gmail.com>
2022-12-14 14:24:41 -0800

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,12 +4,14 @@
  * we check if c is in an array, then output a return value
  * Return: 1 if c is present and 0 if not
  */
-int main(void)
+int main(int)
 {
 int _isalpha(int c)
 
 	if (c >= 'a'; c <= 'z')
 		return (1);
-	else
+	else if (c >= 'A'; c <= 'Z')
+		return (1);
+        else
 		return (0);
 }
