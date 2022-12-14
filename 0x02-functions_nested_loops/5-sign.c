7e0d2398c08bd9ae3791a2fe14913b5f474fe295
Eniade-Oluwademilade <demiladeeniade@gmail.com>
2022-12-14 14:47:58 -0800

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main
+ * printing sign of a number
+ * Return: 1 if positive, 0 if zero, -1 if negative
+ */
+int main(void)
+{
+int print_sign(int n)
+if (n > 0)
+	_putchar("+\n");
+return (1);
+else if (n = 0)
+	_putchar("0\n");
+	return (0);
+else
+	_putchar("-\n");
+	return (-1);
