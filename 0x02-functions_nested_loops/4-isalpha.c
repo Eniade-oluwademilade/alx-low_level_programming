381e6564658df6a79523b5a4a9721d5bfe799402
Eniade-Oluwademilade <demiladeeniade@gmail.com>
2022-12-14 14:16:28 -0800

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * we first declare the main function
+ * we check if c is in an array, then output a return value
+ * Return: 1 if c is present and 0 if not
+ */
+int main(void)
+{
+int _isalpha(int c)
+
+	if (c >= 'a'; c <= 'z')
+		return (1);
+	else
+		return (0);
+}
