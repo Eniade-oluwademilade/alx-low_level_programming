bd5808a1dcbee42becc4518ed160a4d705e861b2
Eniade-Oluwademilade <demiladeeniade@gmail.com>
2022-12-18 09:35:05 -0800

    print all except q & e

---
+++ 0x00-hello_world/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**printing alphabets except q & e
+ * return: 0
+ */
+int main(void)
+{
+	char alpha;
+
+	for((alpha >= 'a'; alpha <= 'z'; alpha++) && !(q && ))
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}
