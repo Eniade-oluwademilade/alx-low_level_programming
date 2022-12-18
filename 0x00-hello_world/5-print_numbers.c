9729a54cfea1b991b70ab4993ac9f6593a5bc1ce
Eniade-Oluwademilade <demiladeeniade@gmail.com>
2022-12-18 09:43:12 -0800

    printing numbers

---
+++ 0x00-hello_world/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * print numbers 1 to 10
+ * returng 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while(i <= 9)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
