92c9b78e46a471702ea45653d5fc4e1ecf131a10
Eniade-Oluwademilade <demiladeeniade@gmail.com>
2022-12-20 10:15:05 -0800

    reversing strings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,22 @@
 #include "main.h"
 /**
- * this function reverses strings
+ *rev_string - this function reverses strings
+ *@s: array string holder
+ *@i: counter
+ *@a: to loop over an array
  */
 void rev_string(char *s)
+{
+	int len = 0, index = 0;
+	char temp;
+
+	while (s[index++])
+		len++;
+
+	for (index = len - 1; index >= len / 2; index--)
+	{
+		tmp = s[index];
+		s[index] = s[len - index - 1];
+		s[len - index - 1] = tmp;
+	}
+}
