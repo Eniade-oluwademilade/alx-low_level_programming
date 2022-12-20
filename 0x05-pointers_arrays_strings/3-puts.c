9d482147c85662426130af7e9898866e3945dfca
Eniade-Oluwademilade <demiladeeniade@gmail.com>
2022-12-20 06:51:05 -0800

    pseudo put function

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,5 +1,15 @@
 #include "main.h"
 /**
- * function prints string and new line
+ *_puts - function prints string and new line
+ *@str: pointer tostring
+ *@i: our counter
  */
 void _puts(char *str)
+{
+	int i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
