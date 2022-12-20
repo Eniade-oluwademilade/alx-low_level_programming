f741106fe61ba95199283aedc204a8872b872a9f
Eniade-Oluwademilade <demiladeeniade@gmail.com>
2022-12-20 10:33:41 -0800

    half string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,6 +1,31 @@
 #include "main.h"
 /**
- * function prints half of sting then a new line
+ *puts_half - function prints half of sting then a new line
  * if length of string is odd n = (length of string - 1) /2
+ *@str: our pointer
  */
 void puts_half(char *str)
+{
+	int j = 0;
+	int k;
+
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+	if (j % 2 == 1)
+	{
+		k = (j - 1) / 2;
+		k += 1;
+	}
+	else
+	{
+		k = j / 2;
+	}
+
+	for (; k < j; k++)
+	{
+		_putchar(str[k]);
+	}
+	_putchar('\n');
+}
