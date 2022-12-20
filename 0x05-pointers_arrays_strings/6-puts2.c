0f54c617f8b3909936583d6dfa2eec345bc1fc26
Eniade-Oluwademilade <demiladeeniade@gmail.com>
2022-12-20 10:23:58 -0800

    every other char

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,22 @@
 #include "main.h"
 /**
- * function prints every other element in string array
- * such as [0, 2, 4, 6] and %2 = 0 array element arr[i]
+ * puts2 - prints every other element in string array
+ * @str: array pointer
+ * e.g [0, 2, 4, 6] and %2 = 0 array element arr[i]
  */
 void puts2(char *str)
+{
+	int i;
+	int j = 0;
+
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+
+	for (i = 0; i < j; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
