31c7031c83490ff46275581daf91fff8b45cfb03
Eniade-Oluwademilade <demiladeeniade@gmail.com>
2023-01-01 13:56:13 -0800

    hello world!

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,11 +6,11 @@
  */
 int main(void)
 {
-printf("size of char: %lu byte(s)\n",(unsigned long)sizeof(char));
+printf("size of char: %lu byte(s)\n", (unsigned long)sizeof(char));
 printf("size of int: %lu byte(s)\n", (unsigned long)sizeof(int));
 printf("size of long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
 printf("size of long long int: %lu byte(s)\n", (unsigned long)sizeof(long long int));
 printf("size of float: %lu byte(s)\n", (unsigned long)sizeof(float));
-return(0);
+return (0);
 }
 
