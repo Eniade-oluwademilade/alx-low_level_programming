efeeb25a39ae4703ea677e84ecc8452a5321841b
Eniade-Oluwademilade <demiladeeniade@gmail.com>
2023-01-03 10:33:55 -0800

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - absolute value of int
+ *@t: integer to compute
+ *Return: absolute value
+ */
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
