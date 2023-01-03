5e7eedf5cc7d475534ad8034e119dd5524f58076
Eniade-Oluwademilade <demiladeeniade@gmail.com>
2023-01-03 11:13:22 -0800

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,12 +6,12 @@
  */
 int print_last_digit(int n)
 {
-	int d = n % 10;
+	int ld = n % 10;
 
-	if (d < 0)
-		d *= -1;
+	if (ld < 0)
+		ld *= -1;
 
-	_putchar(d + '0');
+	_putchar(ld + '0');
 
 	return (0);
 }
