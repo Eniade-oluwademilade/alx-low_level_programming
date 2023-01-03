ccf225af13a02264b4b7fe1dfb08e830f51bbdb0
Eniade-Oluwademilade <demiladeeniade@gmail.com>
2023-01-03 08:02:57 -0800

    array of integers

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -4,54 +4,21 @@
 
 /**
  * main generates random passwords
- * program 101-crackme
+ * program 101-crackeme
  * Return: 0
  */
 int main(void)
-
 {
-	char passwors[90];
-	int index = 0, sum = 0, diff_half1, diff_half2;
+	int pass, sum;
 
-	srand(time(0));
-
-	while (sum < 2772)
+	srand(time(NULL));
+	sum = 0;
+	while (sum <= 2645)
 	{
-		password[index] = 33 + rand() % 94;
-		sum += password[index++];
+		pass = (rand() % 128);
+		sum += pass;
+		printf("%c", pass);
 	}
-	password[index] = '\0';
-
-	if ((sum != 2772)
-	{
-			diff_half1 = (sum - 2772) / 2;
-			diff_half2 = (sum - 2772) / 2;
-
-			if ((sum - 2772) % 2 != 0)
-			diff_half1++;
-
-			for (index = 0; password[index]; index++)
-
-			{
-			if (password[index] >= (33 + diff_half1))
-			}
-
-			password[index] -= diff_half1;
-			break;	
-	}
-
-			for (index = 0; password[index]; index++)
-			{
-				if (password[index] >= (33 + diff_half2))
-
-				{
-					password[index] -= diff_half2;
-
-					break;
-				}
-			}
-
-	}
-	printf("%s", password);
+	printf("%c", 2772 -.sum);
 	return (0);
 }
--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,12 +10,9 @@ void print_array(int *a, int n)
 	int j;
 
 	for (j = 0; j < n; j++)
-	{
-		printf("%d", a[j]);
-				if (j != (n - 1))
-				{
-				printf(", ");
-				}
-				printf("\n");
-				}
+		if (j != n - 1)
+			printf("%d, ", a[j]);
+		else
+			printf("%d", a[j]);
+	printf("\n");
 }
