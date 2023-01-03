098d099cab4431336d93a71bf85a120955a1c88e
Eniade-Oluwademilade <demiladeeniade@gmail.com>
2023-01-03 11:41:21 -0800

    loop in loop

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_to_98 - print all nums
+ * @n: beginning
+ */
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
+}
