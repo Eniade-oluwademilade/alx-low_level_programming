c6b54ceb6dda68f1ec2fd37de47c379c5c44e48c
Eniade-Oluwademilade <demiladeeniade@gmail.com>
2023-01-03 11:01:08 -0800

    Every minute

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,12 +6,12 @@ void jack_bauer(void)
 {
 	int hr, min;
 
-	for (hour = 0; hour <= 23; hour++)
+	for (hr = 0; hr <= 23; hr++)
 	{
 		for (min = 0; min <= 59; min++)
 		{
-			_putchar((hour / 10) + '0');
-			_putchar((hour % 10) + '0');
+			_putchar((hr / 10) + '0');
+			_putchar((hr % 10) + '0');
 			_putchar(';');
 			_putchar((min / 10) + '0');
 			_putchar((min % 10) + '0');
