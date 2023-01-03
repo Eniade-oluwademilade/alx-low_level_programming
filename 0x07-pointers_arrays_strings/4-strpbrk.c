cc70c37d706b7608c42883845ba5129d8088b052
Eniade-Oluwademilade <demiladeeniade@gmail.com>
2023-01-03 07:31:05 -0800

    Even more pointers

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,7 +9,7 @@ char *_strpbrk(char *s, char *accept)
 {
 	unsigned int i, j;
 
-	for (i = 0; *s(i + 1) != '\0'; i++)
+	for (i = 0; *(s + i) != '\0'; i++)
 	{
 		for (j = 0; *(accept + j) != '\0'; j++)
 		{
