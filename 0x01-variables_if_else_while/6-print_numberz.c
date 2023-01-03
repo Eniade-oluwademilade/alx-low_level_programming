a788cd4dbbe1c16606eb7b9a424ab44c8e4acf14
Eniade-Oluwademilade <demiladeeniade@gmail.com>
2023-01-03 02:14:21 -0800

    variables

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Return: 0 always
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/7-print_tebalpha.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 always
+ */
+int main(void)
+{
+	char l;
+
+	for (l = 'z'; l >= 'a'; l++)
+		putchar(l);
+
+	putchar('\n');
+	return (0);
+}
