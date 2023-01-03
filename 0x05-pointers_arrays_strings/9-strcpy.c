307953bc74fb7fe11354440dfa24156df53d40b5
Eniade-Oluwademilade <demiladeeniade@gmail.com>
2023-01-03 08:11:18 -0800

    array of integers

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * _strcpy - copies from src to dest
- * @dest: char 
- . @src: char to check
+ * @dest: char
+ * @src: char to check
  * write function to copy strings from src and \0 to buffer by dest
  * Return: (dest) *checkout man strcpy*
  */
