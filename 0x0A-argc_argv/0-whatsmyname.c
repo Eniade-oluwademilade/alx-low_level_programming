54c1f54dd5bf4457dfd40a7cf1f169e532063f6f
Eniade-Oluwademilade <demiladeeniade@gmail.com>
2023-01-06 04:43:06 -0800

    command line arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point.
+ * @argc: argument count.
+ * @argv: argument vector.
+ * Return: 0 success.
+ */
+int main(int argc, char *argv[])
+{
+	argc = 0;
+	printf("%s\n", argv[argc]);
+	return (0);
+}
