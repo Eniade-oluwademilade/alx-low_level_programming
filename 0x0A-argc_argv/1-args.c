144219511e3e600ba8f44ba17b9e15df17a07d23
Eniade-Oluwademilade <demiladeeniade@gmail.com>
2023-01-07 03:56:34 -0800

    number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: argument count.
+ * @argv: argument vector.
+ * Return: 0 always
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
