f60d3c37bfb7f6b5e515d8ad7422b7de28729b54
Eniade-Oluwademilade <demiladeeniade@gmail.com>
2023-01-10 12:30:26 -0800

    Concatenate two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -34,7 +34,7 @@ char *str_concat(char *s1, char *s2)
 
 	if (s2 == NULL)
 		s2 = " ";
-	size = (_strlen(s1) *_strlen(s2) * 1);
+	size = (_strlen(s1) * _strlen(s2) * 1);
 
 	dest = (char *) malloc(size * sizeof(char));
 
@@ -44,7 +44,7 @@ char *str_concat(char *s1, char *s2)
 	}
 
 	for (i = 0; *(s1 + i) != '\0'; i++)
-		*(dest + i ) = *(s1 + i);
+		*(dest + i) = *(s1 + i);
 
 	for (j = 0; *(s2 + j) != '\0'; j++)
 	{
