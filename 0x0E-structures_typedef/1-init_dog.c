8f157edd854a3af164eca7f3b9f6c68e5d642626
Eniade-Oluwademilade <demiladeeniade@gmail.com>
2023-01-17 18:25:53 -0800

    Puppy bio structure

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * init_dog - to initialize dog's info
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+{
+	if (d != NULL)
+	{
+		(*d).name = name;
+		(*d).age = age;
+		(*d).owner = owner;
+	}
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,24 @@
-#include <stdio.h>
+#ifndef DOG_H
+#define DOG_H
 
+/**
+ * struct dog - stucture for 'dog'
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Description: structure holds dog's info
+ */
 struct dog
 {
 	char* name;
 	float age;
 	char* owner;
 };
+
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
