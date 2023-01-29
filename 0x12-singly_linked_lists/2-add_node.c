d9bd38087f59f13701f4013aa6c324b437decca7
Eniade-Oluwademilade <demiladeeniade@gmail.com>
2023-01-29 10:51:33 -0800

    singly linked list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 /**
  * add_node - inserts a node 
  * @head: start of node
@@ -10,5 +11,29 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
+	char *copy;
+	int len;
+	list_t *new;
 
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	copy = strdup(str);
+	if (copy == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	for (len = 0; str[len];)
+		len++;
+
+	new->str = copy;
+	new->len = len;
+	new->next = *head;
+
+	*head = new;
+
+	return (new);
 }
