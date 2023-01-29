d92af746284e993e1dc88b69126a420dbb5f6b9f
Eniade-Oluwademilade <demiladeeniade@gmail.com>
2023-01-29 09:55:32 -0800

    singly linked list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * add_node - inserts a node 
+ * @head: start of node
+ * @str: list
+ *
+ * Return: address of new element or NULL.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+
+}
