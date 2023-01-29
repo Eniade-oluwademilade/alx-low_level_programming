85f88d2ecbddde39913ba3c303145d896afcf3db
Eniade-Oluwademilade <demiladeeniade@gmail.com>
2023-01-29 12:23:52 -0800

    singly linked lists

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include  <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+#include <string.h>
+/**
+ * free_list - frees list
+ * @head: start of node
+ */
+void free_list(list_t **head)
+{
+	list_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
+}
