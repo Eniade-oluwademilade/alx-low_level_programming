9fa69c5f5c9ba2f8dc07f930746aaea4388bebef
Eniade-Oluwademilade <demiladeeniade@gmail.com>
2023-01-31 15:23:48 -0800

    more linked lists

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,7 +1,6 @@
 #include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 /**
  * free_listint2 - the function frees a list
  * @head: start of node
@@ -10,5 +9,17 @@
  */
 void free_listint2(listint_t **head)
 {
+	listint_t *temp;
 
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head =  temp;
+	}
+
+	head = NULL;
 }
