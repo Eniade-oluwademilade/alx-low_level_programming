8d9c91faf4f096383f0f0407981adc947bcba927
Eniade-Oluwademilade <demiladeeniade@gmail.com>
2023-01-31 15:33:28 -0800

    more linked lists

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,7 +1,6 @@
 #include "lists"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 /**
  * get_nodeint_at_index - returns nth node of list
  * @head: start of node 
@@ -11,4 +10,15 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
+	unsigned int i;
+
+	for (i = 0; i < index; i++)
+	{
+		if (head == NULL)
+			return (NULL);
+
+		head = head->next;
+	}
+
+	return (head);
 }
