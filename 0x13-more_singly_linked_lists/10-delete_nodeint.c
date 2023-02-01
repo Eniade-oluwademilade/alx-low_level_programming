e223f186cb5fdf788b0e315e12a611423ac803f4
Eniade-Oluwademilade <demiladeeniade@gmail.com>
2023-01-31 16:37:28 -0800

    more singly linked lists

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,7 +13,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *temp, *cpy = *head;
 	unsigned int node;
 
-	if (copy == NULL)
+	if (cpy == NULL)
 		return (-1);
 
 	if(index == 0)
