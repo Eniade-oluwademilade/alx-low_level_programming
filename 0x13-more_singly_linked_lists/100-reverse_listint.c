339c742df7995f200ac1265a210bd07af4831f94
Eniade-Oluwademilade <demiladeeniade@gmail.com>
2023-01-31 16:18:35 -0800

    more singly linked lists

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -10,4 +10,22 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
+	listint_t *front, *back;
+
+	if (head == NULL || *head == NULL)
+		return (NULL);
+
+	back = NULL;
+
+	while ((*head)->next != NULL)
+	{
+		front = (*head)->next;
+		(*head)->next = back;
+		back = *head;
+		*head = front;
+	}
+
+	(*head)->next = back;
+
+	return (*head);
 }
